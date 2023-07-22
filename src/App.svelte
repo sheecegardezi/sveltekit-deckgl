bbd22f6a57ca7c130cff8f2f10861d52212410ae
Sheece Gardezi <sheecegardezi@gmail.com>
2023-07-23 02:34:17 +1000

    create bare minimum example

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,67 @@
 <script>
-    import { default as SvelteDeckGL } from './components/Deckgl.svelte';
-    
-    const MAPBOX_TOKEN = "pk.eyJ1IjoiYXJwZXJ5YW4iLCJhIjoiY2l4cTJkc2t6MDAzcjJxcG9maWp1ZmFjMCJ9.XT957ywrTABjNFqGdp_37g";
+    import { onMount } from "svelte";
+    import { Deck } from "@deck.gl/core";
+    import { GeoJsonLayer } from "@deck.gl/layers";
 
-    let deckInstance;
-
-
-    function getTooltip({ object }) {
-        if (!object) {
-            return null;
-        }
-
-        const count = object.points.length;
-
-        return object && `No. of bikes: ${count} `;
-    }
     let viewState = {
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 11.2,
-        maxZoom: 20,
-        pitch: 50,
-        bearing: -60,
-        style: "mapbox://styles/arperyan/cj8n10t397cyn2rkap4ybrjid",
+        latitude: 51.47,
+        longitude: 0.45,
+        zoom: 4,
+        bearing: 0,
+        pitch: 30,
     };
 
+    let deck = null;
+    let deckCanvas = null;
 
+    onMount(() => {
+        render();
+    });
+
+    function render() {
+        const INITIAL_VIEW_STATE = {
+            latitude: 51.47,
+            longitude: 0.45,
+            zoom: 4,
+            bearing: 0,
+            pitch: 30,
+            ...viewState,
+        };
+
+        deck = new Deck({
+            canvas: deckCanvas,
+            width: "100%",
+            height: "100%",
+            initialViewState: INITIAL_VIEW_STATE,
+            controller: true,
+            layers: [
+                new GeoJsonLayer({
+                    id: "base-map",
+                    data: "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_scale_rank.geojson",
+                    // Styles
+					stroked: true,
+					filled: true,
+					lineWidthMinPixels: 2,
+					opacity: 0.4,
+					getLineColor: [60, 60, 60],
+					getFillColor: [200, 200, 200]
+                }),
+            ],
+        });
+    }
 </script>
 
 <div class="App">
-    <SvelteDeckGL {viewState} {getTooltip} TOKEN={MAPBOX_TOKEN} bind:deck={deckInstance} />
+    <canvas class="deck-canvas" bind:this={deckCanvas} />
 </div>
 
 <style>
     .App {
         height: 100vh;
         width: 100vw;
+    }
+    .deck-canvas {
+        width: 100%;
+        height: 100%;
     }
 </style>
